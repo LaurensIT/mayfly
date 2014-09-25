71bb65d84d9ecaac0d20a7936f1e5e682cdaf98f
Jan-Jorre Laurens <janjorre.laurens@cyso.com>
2014-09-25 15:44:09 +0200

    Added license declerations.

--- app/lang/de/onetime.php
+++ app/lang/de/onetime.php
@@ -1,5 +1,22 @@
 <?php
-        return array(
+/*
+This file is part of Mayfly.
+
+Mayfly is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Foobar is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+return array(
                 'createlink' => 'Verknüpfung erstellen',
                 'E-mail' => 'E-mail',
                 'Your name' => 'Ihr name',
